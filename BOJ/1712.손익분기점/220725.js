54e3f7fc5a0068f99409b6d2ffe9da21df628cd1
Ok-Cheese <starberry970310@gmail.com>
2022-07-25 16:49:21 +0900

    [BOJ]1712.손익분기점

---
+++ BOJ/1712.손익분기점/220725.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const [a, b, c] = fs.readFileSync("dev/stdin").toString().split(" ");
+
+const profit = c - b;
+
+if (profit <= 0) {
+  console.log(-1);
+} else {
+  const BEP = a / profit;
+  console.log(a % profit ? Math.ceil(BEP) : Math.ceil(BEP) + 1);
+}
