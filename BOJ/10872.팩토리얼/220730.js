38c3ecfb250ca577ba5994ad8ec392185af2d713
Ok-Cheese <starberry970310@gmail.com>
2022-07-30 21:13:00 +0900

    [BOJ]10872.팩토리얼

---
+++ BOJ/10872.팩토리얼/220730.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync("dev/stdin").toString());
+
+function factorial(num) {
+  if (num === 0 || num === 1) return 1;
+
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(n));
