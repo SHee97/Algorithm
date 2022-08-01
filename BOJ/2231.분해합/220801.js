58b69a52267ffd346ab6ff92bc54294a98bed97a
Ok-Cheese <starberry970310@gmail.com>
2022-08-01 10:06:35 +0900

    [BOJ]2231.분해합

---
+++ BOJ/2231.분해합/220801.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const n = +fs.readFileSync("dev/stdin").toString();
+
+let answer = 0;
+for (let i = 1; i < n; i++) {
+  let sum = i;
+  let num = i.toString();
+  for (let j of num) {
+    sum += +j;
+  }
+
+  if (sum === n) {
+    answer = i;
+    break;
+  }
+}
+
+console.log(answer);
