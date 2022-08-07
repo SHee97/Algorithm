4ce9a96a70ce7d815d0d3011189b6a8c05f8df96
Ok-Cheese <starberry970310@gmail.com>
2022-08-07 10:31:35 +0900

    8.일곱 난쟁이

---
+++ JS_Algorithm/section1/8.일곱난쟁이.js
@@ -0,0 +1,16 @@
+const snowWhite = (input) => {
+  const answer = input.slice();
+  const sum = answer.reduce((acc, e) => acc + e, 0);
+
+  for (let i = 0; i < answer.length; i++) {
+    for (let j = i + 1; j < answer.length; j++) {
+      if (sum - (answer[i] + answer[j]) === 100) {
+        answer.splice(i, 1);
+        answer.splice(j - 1, 1);
+        return answer;
+      }
+    }
+  }
+};
+
+console.log(snowWhite([20, 7, 23, 19, 10, 15, 25, 8, 13]));
