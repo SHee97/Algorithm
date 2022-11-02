436095f15b9bc1f2ce9b3daf6e58796b47f46908
unknown <starberry970310@gmail.com>
2022-11-02 16:16:51 +0900

    lv.2 가장 큰 수

---
+++ Programmers/lv.2/가장큰수.js
@@ -0,0 +1,10 @@
+function solution(numbers) {
+  const answer = numbers.map(e => e.toString()).sort((a, b) => {
+      const A = a + b;
+      const B = b + a;
+      
+      return +(B) - +(A);
+  });
+  
+  return (BigInt(answer.join(''))).toString();
+}
