42d6a29cb8127659d7d83773a08750e79224c742
Seokhee Cho <90900744+SHee97@users.noreply.github.com>
2022-11-19 12:13:59 +0900

    [level 1] Title: 행렬의 덧셈, Time: 7.99 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,15 @@
+function solution(arr1, arr2) {
+    const answer = [];
+    
+    for (let i = 0; i < arr1.length; i++) {
+        const element = [];
+        
+        for (let  j = 0; j < arr1[i].length; j++) {
+            element.push(arr1[i][j] + arr2[i][j]);
+        }
+        
+        answer.push(element);
+    }
+    
+    return answer;
+}
