56e6b173e830a00cc31caa8c49c7d4e307168dbc
Seokhee Cho <90900744+SHee97@users.noreply.github.com>
2022-11-21 14:51:20 +0900

    [level 1] Title: 폰켓몬, Time: 1.00 ms, Memory: 34.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -0,0 +1,12 @@
+function solution(nums) {
+    const set = new Set();
+    const max = nums.length / 2;
+    
+    for (let n of nums) {
+        set.add(n);
+    };
+    
+    const sp = set.size;
+    
+    return Math.min(sp, max);
+}
