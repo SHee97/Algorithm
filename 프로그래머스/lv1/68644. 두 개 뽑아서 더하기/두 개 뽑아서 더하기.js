1343b7cb4504721958432566e2e7d33ec3153544
Seokhee Cho <90900744+SHee97@users.noreply.github.com>
2022-11-21 13:05:03 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.80 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const answer = [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < numbers.length; j++) {
+            if (i === j) continue;
+            
+            answer.push(numbers[i] + numbers[j]);
+        }
+    }
+    
+    return [...new Set(answer)].sort((a, b) => a - b);
+}
