7797ece98883831e368f955f5c126adae8f2d4a3
Seokhee Cho <90900744+SHee97@users.noreply.github.com>
2022-11-25 19:47:57 +0900

    Delete BOJ directory

--- BOJ/10757.큰수A+B/220728.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs");
-const [a, b] = fs.readFileSync("dev/stdin").toString().split(" ").map(BigInt);
-
-console.log((a + b).toString());
--- BOJ/2869.달팽이는올라가고싶다/220726.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const [a, b, v] = fs.readFileSync("dev/stdin").toString().split(" ");
-
-const last = v - a;
-const result = last % (a - b) ? last / (a - b) + 1 : last / (a - b);
-console.log(Math.floor(result) + 1);
